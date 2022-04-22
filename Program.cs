82e63b38670429037917c3d53b5c48a202f1f78f
Ludovic Labbé <ludovic.labbe@elca.ch>
2022-04-22 17:18:20 +0200

    first commit on new feature branch

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     // adding stuff to commit to new branch and more and more useless comments
+    // and it's just the beginning, i'm getting f... ing bored
     app.UseHsts();
 }
 
